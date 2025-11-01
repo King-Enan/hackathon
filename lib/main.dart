fbcbcb7a43a516cf55ff49bfafaa206facda8e17
King-Enan <istihadangkit@gmail.com>
2025-11-01 12:13:54 +0600

    Upload

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
 
         useMaterial3: true,
       ),
+      debugShowCheckedModeBanner: false,
       home: const BuyerHomepage(),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
-import firebase_database
+import file_selector_macos
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
