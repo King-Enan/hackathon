dea7ba25f9f3e0dc06681bb985f3616c8fe6d99b
King-Enan <istihadangkit@gmail.com>
2025-11-01 12:17:59 +0600

    Upload

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
 
         useMaterial3: true,
       ),
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: const BuyerHomepage(),
     );
   }
